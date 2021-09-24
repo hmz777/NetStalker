c0c7a82ef2e494be12b21a1920d5ca7784f15c62
HMZ <hamzi1995@gmail.com>
2021-09-25 00:46:19 +0300

    Add the set name dialog for setting devices' friendly names.

---
+++ NetStalker/NetStalker/Forms/Main/SetNameDialog.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace NetStalker
+{
+    partial class SetNameDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SetNameDialog));
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.ButtonIcons = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(78, 46);
+            this.NameBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(326, 26);
+            this.NameBox.TabIndex = 0;
+            // 
+            // OkButton
+            // 
+            this.OkButton.BackColor = System.Drawing.Color.White;
+            this.OkButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.OkButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.OkButton.FlatAppearance.BorderSize = 0;
+            this.OkButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OkButton.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OkButton.ImageKey = "color_ok.PNG";
+            this.OkButton.ImageList = this.ButtonIcons;
+            this.OkButton.Location = new System.Drawing.Point(485, 27);
+            this.OkButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(119, 67);
+            this.OkButton.TabIndex = 64;
+            this.OkButton.Text = "SET";
+            this.OkButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.OkButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.OkButton.UseVisualStyleBackColor = false;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // ButtonIcons
+            // 
+            this.ButtonIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ButtonIcons.ImageStream")));
+            this.ButtonIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.ButtonIcons.Images.SetKeyName(0, "color_cancel.PNG");
+            this.ButtonIcons.Images.SetKeyName(1, "color_ok.PNG");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 65;
+            this.label1.Text = "Name";
+            // 
+            // SetNameDialog
+            // 
+            this.AcceptButton = this.OkButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(616, 112);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.NameBox);
+            this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SetNameDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Set Friendly Name";
+            this.Load += new System.EventHandler(this.SetNameDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.ImageList ButtonIcons;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox NameBox;
+    }
+}
---
+++ NetStalker/NetStalker/Forms/Main/SetNameDialog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetStalker
+{
+    public partial class SetNameDialog : Form
+    {
+        private readonly string name;
+
+        public SetNameDialog(string Name)
+        {
+            InitializeComponent();
+
+            name = Name;
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(NameBox.Text))
+            {
+                MessageBox.Show(this, Properties.Resources.FriendlyNameEmpty, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (NameBox.Text.Length > 20)
+            {
+                MessageBox.Show(this, Properties.Resources.FriendlyNameCharLimit, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void SetNameDialog_Load(object sender, EventArgs e)
+        {
+            NameBox.Text = name;
+        }
+    }
+}
